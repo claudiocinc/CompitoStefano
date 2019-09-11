87749305dcde5b6de8e6670b34f006e7f1ceb172
claudiocinc <54982760+claudiocinc@users.noreply.github.com>
2019-09-11 17:45:25 +0200

    Sistemare form html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
+import { EditPersonComponent } from './home-page/edit-person/edit-person.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: ':id/edit', component: EditPersonComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,49 @@
+import { HttpClient, HttpHandler, HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AccordionModule } from 'primeng/components/accordion/accordion';
+import {OrderListModule} from 'primeng/orderlist';
+import { HomePageComponent } from './home-page/home-page.component';
+import {InputTextModule} from 'primeng/inputtext';
+import {MultiSelectModule} from 'primeng/multiselect';
+import { FormsModule } from '@angular/forms';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {TableModule} from 'primeng/table';
+import {ButtonModule} from 'primeng/button';
+import { EditPersonComponent } from './home-page/edit-person/edit-person.component';
+import {DialogModule} from 'primeng/dialog';
+import {CalendarModule} from 'primeng/calendar';
+import {DropdownModule} from 'primeng/dropdown';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    EditPersonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AccordionModule,
+    OrderListModule,
+    InputTextModule,
+    MultiSelectModule,
+    FormsModule,
+    RadioButtonModule,
+    TableModule,
+    ButtonModule,
+    HttpClientModule,
+    DialogModule,
+    CalendarModule,
+    DropdownModule
   ],
-  providers: [],
+  providers: [HttpClient],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
